e0db5ebc8c1ba105281b61e0452163634d05cf91
Johan Bernhardsson <johan@johanbernhardsson.se>
2021-12-07 06:17:44 +0100

    Day07 first gold star :)

---
+++ Day07/Day07.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "../Lib/CommonLib.h"
+
+//const char* InputData = "TestInputData.txt";
+const char* InputData = "MyInput.txt";
+
+int main(int argc, char* argv[])
+{
+    Timer fullTime("Full time");
+
+	std::ifstream inputDataStream;
+    inputDataStream.open(InputData);
+    
+    if (!inputDataStream.is_open())
+    {
+        std::cerr << "Incorrect path to input data" << std::endl;
+        return 1;
+    }
+
+    std::vector<uint32_t> crabStartPositions;
+    std::string buffer;
+    
+    while (std::getline(inputDataStream, buffer, ','))
+    {
+		crabStartPositions.push_back(std::atoi(buffer.c_str()));
+	}
+
+    std::sort(crabStartPositions.begin(), crabStartPositions.end());
+
+    const uint32_t maxNumber = *(crabStartPositions.end() - 1);
+    uint64_t minCost = std::numeric_limits<uint64_t>::max();
+    int32_t bestPosition = std::numeric_limits<int32_t>::min();;
+    for (int32_t x = crabStartPositions[0]; x <= maxNumber; ++x)
+    {
+        uint64_t count = 0;
+        for (const int32_t crabStartPosition : crabStartPositions)
+        {
+            count += std::abs(crabStartPosition - x);
+        }
+        if (count < minCost)
+        {
+            minCost = count;
+            bestPosition = x;
+        }
+    }
+
+    std::cout << "Best position is " << bestPosition << " with cost " << minCost << std::endl;
+    
+    return 0;
+}
